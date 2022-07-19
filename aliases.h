128c8481c815244481b62679dd2e9539158519a9
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-07-19 03:06:44 +0100

    add symbols layer

--- aliases.h
+++ aliases.h
@@ -23,6 +23,32 @@
 #define ______________NUMBER_LEFT_ALT______________       KC_6,    KC_7,    KC_8,    KC_9,    KC_LBRC
 #define ______________NUMBER_RIGHT_ALT_____________       KC_RBRC, KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5
 
+// my custom layers
+#define _________________KEYPAD_R0_________________       ____,    KC_NUM,  KC_SLSH, KC_ASTR, KC_MINS
+#define _________________KEYPAD_R1_________________       ____,    KC_7,    KC_8,    KC_9,    KC_MINS
+#define _________________KEYPAD_R2_________________       ____,    KC_4,    KC_5,    KC_6,    KC_PLUS
+#define _________________KEYPAD_R3_________________       ____,    KC_1,    KC_2,    KC_3,    KC_PLUS
+
+#define _______________FUNCTIONS_L0________________       DF(_UCIEA), DF(_UCIEA_GCSV), DF(_ENGRAM), ____,    ____
+#define _______________FUNCTIONS_L1________________       ____,       RGB_VAI,         ____,        ____,    ____
+#define _______________FUNCTIONS_L2________________       ____,       RGB_VAD,         ____,        ____,    ____
+#define _______________FUNCTIONS_L3________________       ___________________BLANK___________________
+
+#define _______________FUNCTIONS_R0________________       ___________________BLANK___________________
+#define _______________FUNCTIONS_R1________________       ___________________BLANK___________________
+#define _______________FUNCTIONS_R2________________       ___________________BLANK___________________
+#define _______________FUNCTIONS_R3________________       NK_TOGG, ____,     ____,     ____,     ____
+
+#define ________________SYMBOLS_L1_________________       XXXX,    KC_EXLM, KC_EQL,  KC_MINS, KC_ASTR
+#define ________________SYMBOLS_L2_________________       XXXX,    KC_LBRC, KC_LCBR, KC_LPRN, KC_QUOT
+#define ________________SYMBOLS_L3_________________       XXXX,    XXXX,    XXXX,    KC_LT,   KC_AMPR
+
+#define ________________SYMBOLS_R1_________________       KC_SLSH, KC_BSLS, KC_DLR,  KC_HASH, XXXX
+#define ________________SYMBOLS_R2_________________       KC_DQUO, KC_RPRN, KC_RCBR, KC_RBRC, XXXX
+#define ________________SYMBOLS_R3_________________       KC_PIPE, KC_GT,   XXXX,    XXXX,    XXXX
+
+
+// standard keyboard typing layouts, with some layer mods
 #define _________________QWERTY_L1_________________       KC_Q,      KC_W,    KC_E,    KC_R,    KC_T
 #define _________________QWERTY_L2_________________       SYM(KC_A), KC_S,    KC_D,    KC_F,    KC_G
 #define _________________QWERTY_L3_________________       KC_Z,      KC_X,    KC_C,    KC_V,    KC_B
@@ -55,17 +81,3 @@
 #define _________________ENGRAM_R2_________________       KC_DOT,  KC_H,    KC_T,    KC_S,    KC_N,    SYM(KC_Q)
 #define _________________ENGRAM_R3_________________       KC_QUES, KC_R,    KC_M,    KC_F,    KC_P
 
-#define _________________KEYPAD_R0_________________       ____,    KC_NUM,  KC_SLSH, KC_ASTR, KC_MINS
-#define _________________KEYPAD_R1_________________       ____,    KC_7,    KC_8,    KC_9,    KC_MINS
-#define _________________KEYPAD_R2_________________       ____,    KC_4,    KC_5,    KC_6,    KC_PLUS
-#define _________________KEYPAD_R3_________________       ____,    KC_1,    KC_2,    KC_3,    KC_PLUS
-
-#define _______________FUNCTIONS_L0________________       DF(_UCIEA), DF(_UCIEA_GCSV), DF(_ENGRAM), ____,    ____
-#define _______________FUNCTIONS_L1________________       ____,       RGB_VAI,         ____,        ____,    ____
-#define _______________FUNCTIONS_L2________________       ____,       RGB_VAD,         ____,        ____,    ____
-#define _______________FUNCTIONS_L3________________       ___________________BLANK___________________
-
-#define _______________FUNCTIONS_R0________________       ___________________BLANK___________________
-#define _______________FUNCTIONS_R1________________       ___________________BLANK___________________
-#define _______________FUNCTIONS_R2________________       ___________________BLANK___________________
-#define _______________FUNCTIONS_R3________________       NK_TOGG, ____,     ____,     ____,     ____
